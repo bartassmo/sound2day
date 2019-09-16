917bf3246b044adc9ffe94f71190f795d7e9b122
Bartłomiej Smołucha <bartlomiej.smolucha@gmail.com>
2019-09-16 22:52:25 +0200

    Added sidebar for SeeAlso

--- wp-content/themes/sound2day/templates/single-service.php
+++ wp-content/themes/sound2day/templates/single-service.php
@@ -14,8 +14,30 @@
         </div>
     </div>
 </section>
-<section class="service bPadding">
-    
+<section class="services bPadding">
+<div class="container">
+    <?php while ( have_posts() ) :
+        the_post();
+        the_content();
+    endwhile;
+    ?>
+</div>
+<aside class="sidebar bPadding">
+    <div class="sidebar__content">
+    <?php
+        $current = get_the_id();
+        $seeAlso = new WP_Query( array(
+            'post_type' => 'service',
+            'posts_per_page' => -1,
+            'orderby' => 'random',
+            'post__not_in'  => array($current),
+            ) );
+            ?>
+    <?php while( $seeAlso->have_posts() ) : $seeAlso->the_post();
+        echo get_the_title();
+    endwhile; wp_reset_query(); ?>
+    </div>
+</aside>
 </section>
 
 <?php include(locate_template('footer.php')); ?> 
